452b8d492c3a16b8ff4015bced72f5b4abff0c90
TakashiSan <takashi_fujita_web@icloud.com>
2020-06-13 16:48:23 +0900

    init

---
+++ app/src/main/java/com/example/listmakerw/ListSelectionRecyclerViewAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.listmakerw
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionRecyclerViewAdapter(private val lists:ArrayList<TaskList>) : RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+    private val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder, parent, false)
+        return ListSelectionViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return lists.size
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        holder.listPosition.text = (position + 1).toString()
+        holder.listTitle.text = lists.get(position).name
+    }
+
+    fun addList(list: TaskList) {
+        lists.add(list)
+        notifyItemInserted(lists.size - 1)
+    }
+}
---
+++ app/src/main/java/com/example/listmakerw/TaskList.kt
@@ -0,0 +1,6 @@
+package com.example.listmakerw
+
+class TaskList(val name: String, val tasks: ArrayList<String> = ArrayList()) {
+
+
+}
